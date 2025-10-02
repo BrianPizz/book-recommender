d0ec8d755a4d37d69506972f68573ed84d18ca78
BrianPizz <bpizzimentijr@outlook.com>
2025-10-02 11:29:40 -0400

    add dashboard components

--- gradio-dashboard.py
+++ gradio-dashboard.py
@@ -22,12 +22,12 @@ books["large_thumbnail"] = np.where(
 raw_documents = TextLoader("tagged_description.txt").load()
 text_splitter = CharacterTextSplitter(separator="\n", chunk_size=0, chunk_overlap=0)
 documents = text_splitter.split_documents(raw_documents)
-db_books = Chroma.from_documents(documents, huggingface_embeddings=HuggingFaceEmbeddings())
+db_books = Chroma.from_documents(documents, HuggingFaceEmbeddings())
 
 def retrieve_semantic_recommendations(
         query: str,
         category: str = None,
-        tone: str = None,,
+        tone: str = None,
         initial_top_k: int = 50,
         final_top_k: int = 16,
 ) -> pd.DataFrame:
@@ -53,3 +53,51 @@ def retrieve_semantic_recommendations(
 
     return book_recs
 
+def recommend_books(
+        query: str,
+        category: str,
+        tone: str
+):
+    recommendations = retrieve_semantic_recommendations(query, category, tone)
+    results = []
+
+    for _, row in recommendations.iterrows():
+        description = row["description"]
+        truncated_desc_split = description.split()
+        truncated_description = " ".join(truncated_desc_split[:30]) + "..."
+
+        authors_split = row["authors"].split(";")
+        if len(authors_split) == 2:
+            authors_str = f"{authors_split[0]} and {authors_split[1]}"
+        elif len(authors_split) > 2:
+            authors_str = f"{', '.join(authors_split[:-1])}, and {authors_split[-1]}"
+        else:
+            authors_str = row["authors"]
+
+        caption = f"{row['title']} by {authors_str}: {truncated_description}"
+        results.append((row["large_thumbnail"], caption))
+    return results
+
+categories = ["All"] + sorted(books["simple_categories"].unique())
+tones = ["All"] + ["Happy", "Surprising", "Angry", "Suspenseful", "Sad"]
+
+with gr.Blocks(theme = gr.themes.Glass()) as dashboard:
+    gr.Markdown("# Semantic book recommender")
+
+    with gr.Row():
+        user_query = gr.Textbox(label = "Please enter a description of a book:",
+                                placeholder = "e.g., A story about forgiveness")
+        category_dropdown = gr.Dropdown(choices = categories, label = "Select a category:", value = "All")
+        tone_dropdown = gr.Dropdown(choices = tones, label = "Select an emotional tone:", value = "All")
+        submit_button = gr.Button("Find recommendations")
+
+    gr.Markdown("## Recommendations")
+    output = gr.Gallery(label = "Recommended books", columns = 8, rows = 2)
+
+    submit_button.click(fn = recommend_books,
+                        inputs = [user_query, category_dropdown, tone_dropdown],
+                        outputs = output)
+
+
+if __name__ == "__main__":
+    dashboard.launch()
